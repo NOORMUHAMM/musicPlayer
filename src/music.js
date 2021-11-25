d0d2b104aa1d695c3d658d1193a6a6929c644d41
NOOR <noormuhammed14@gmail.com>
2021-11-26 05:24:07 +0530

    progress bar done

--- src/music.js
+++ src/music.js
@@ -6,7 +6,10 @@ const artist=document.getElementById("artist");
 const title=document.getElementById("title");
 const previous=document.getElementById("previous");
 const next=document.getElementById("next");
-
+const progress = document.getElementById("progress");
+const current_time = document.getElementById("currentTime");
+let duretion = document.getElementById("duretion");
+let current= document.getElementById("currentTime");
 // songs data with artist 
 const song = [
   {
@@ -63,5 +66,31 @@ const preSong=()=>{
           loading(song[songIndex]);
           musicPlay()
 }
+
+
+music.addEventListener("timeupdate",(event)=>{
+  console.log(event)
+  const { currentTime, duration } = event.srcElement;
+  console.log(duration)
+  const progressTime=(currentTime/duration)*100;
+  console.log(progressTime);
+  progress.style.width=`${progressTime}%`
+  
+  let minDuretion=Math.floor(duration/60);
+  let sec = Math.floor(duration % 60);
+  let total=`${minDuretion}:${sec}`
+  if(duration){
+     duretion.textContent = `${total}`;
+  }
+    let min = Math.floor(currentTime / 60);
+    let second = Math.floor(currentTime % 60);
+    let totalTime = `${min}:${second}`;
+    if (duration) {
+      current.textContent = `${totalTime}`;
+    }
+
+})
+
+
 next.addEventListener("click",nextSong);
 previous.addEventListener("click",preSong)
