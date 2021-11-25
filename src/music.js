7c5218a1dec85710b801656a75ad6c15ae6a66fe
NOOR <noormuhammed14@gmail.com>
2021-11-25 22:24:05 +0530

    comment added with js file

--- src/music.js
+++ src/music.js
@@ -1,3 +1,4 @@
+// all element and tag id
 const music=document.querySelector("audio");
 const img=document.querySelector("img");
 const play=document.getElementById("play");
@@ -5,6 +6,8 @@ const artist=document.getElementById("artist");
 const title=document.getElementById("title");
 const previous=document.getElementById("previous");
 const next=document.getElementById("next");
+
+// songs data with artist 
 const song = [
   {
     name: "tu",
@@ -18,19 +21,28 @@ const song = [
   },
 ];
 var isPlay=false;
+
+//  music play function
 const musicPlay=()=>{
     isPlay=true;
     music.play();
     play.classList.replace("fa-play","fa-pause");
 }
+
+//  music stop function
 const stopMusic=()=>{
     isPlay=false;
     music.pause();
     play.classList.replace("fa-pause","fa-play");
 }
+
+// function for play and stop
+
 play.addEventListener("click",()=>{
     isPlay? stopMusic():musicPlay();
 })
+
+// function for load data 
 const loading=(e)=>{
     title.textContent=e.title;
     artist.textContent=e.artist;
@@ -38,11 +50,14 @@ const loading=(e)=>{
     img.src=`image/${e.name}.jpg`
 }
 var songIndex=0;
+
+// for next song functionality
 const nextSong=()=>{
     songIndex=(songIndex+1)%song.length;
     loading(song[songIndex]);
     musicPlay()
 }
+// for previous song functionality
 const preSong=()=>{
        songIndex = (songIndex-1+song.length) % song.length;
           loading(song[songIndex]);
